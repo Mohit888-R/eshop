c99c1944f6ab1bf10021c1651b73573e96e7a5c0
Mohit888-R <naruka.mohit88@gmail.com>
2023-02-17 10:48:59 +0530

    create folder in model and controller

--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,9 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const crypto = require("crypto");
+
 
 const UserSchema = new mongoose.Schema({
     email:{
